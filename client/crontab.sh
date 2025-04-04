2703eff9a66a8885194feaf8f8b8770c4bcf393d
Adel2k <adelineeminian13@gmail.com>
2025-04-04 14:49:53 +0400

    fix cron

--- client/crontab.sh
+++ client/crontab.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-CRON_JOB="@reboot /usr/bin/nohup /usr/bin/python3 /home/potato/Desktop/PhantomStrike/client/is_running.py >> /home/potato/cron.log 2>&1 &"
+CRON_JOB="@reboot $(which nohup) $(which python3) $(find / -name is_running.py) >> /home/$(echo $USER)/cron.log 2>&1 &"
 
 crontab -l | grep -F "$CRON_JOB" > /dev/null
 if [ $? -eq 0 ]; then
